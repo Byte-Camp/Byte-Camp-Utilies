f27f4914d3a24e6e9a1f2fe9f4c108ff4ed220ad
brandoshizzle <cathcart.brandon@gmail.com>
2017-05-16 13:12:04 -0600

    Added videoEncoder.py

---
+++ videoEncoder.py
@@ -0,0 +1,45 @@
+"""
+Byte Camp Image Sequence to Video Encoder
+Requires FFMPEG and Python 2
+"""
+__version__ = '1.0'
+__author__ = 'Adar Guy and Brandon Cathcart'
+
+
+import sys, os, subprocess
+from time import sleep
+
+print ("Welcome to Byte Camp's Video Encoder.\n")
+filepath = raw_input("Drag-n-Drop the first JPG or PNG image ending in '0000.jpg' or similar\nAnd press ENTER to make your video!\n")
+if (filepath[0] == "\"" and filepath[-1] == "\""):
+	filepath = filepath[1:-1]
+
+seq = filepath[-5]; c = 0; i = 1;
+while seq.isdigit():
+	c += 1
+	try:
+		seq = filepath[-5-c]
+	except Exception:
+		filepath = filepath[:-4-c]+"student"+filepath[-4-c:]
+
+try:
+	startNum = int(filepath[-5])
+except Exception:
+	startNum = -1
+
+if (startNum >= 0):
+	if (filepath[-4:] == ".png" or filepath[-4:] == ".jpg"):
+		if (filepath[-4:] == ".png"):
+			filename = filepath[:-4-c]+"%0"+str(c)+"d.png"
+		else:
+			filename = filepath[:-4-c]+"%0"+str(c)+"d.png"
+		subprocess.call(["ffmpeg","-f","image2","-r","12","-start_number",str(startNum),"-i",filename,"-vcodec","libx264",filepath[:-8]+".mp4", "-y"])
+		print ("Conversion Completed Successfully! Enjoy your Video :D")
+		sleep(5)
+	else:
+		print ("The filetype is not PNG or JPG. Exiting Program.")
+		sleep(5)
+		
+else:
+	print ("The filename does not end in a beginning sequence (ex: example0001.png)")
+	sleep(5)
